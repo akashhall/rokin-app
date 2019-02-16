66e9ca5faba20becaa1f494e4b90cde5eef11420
akashhall <41614662+akashhall@users.noreply.github.com>
2019-02-16 18:50:56 +0530

    Update index.js
    updates

--- src/components/Pages/LoginPage/index.js
+++ src/components/Pages/LoginPage/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import TextField from '@material-ui/core/TextField';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import ArrowForward from '@material-ui/icons/ArrowForward';
-import styles from './user-jss';
-import PapperBlock from './../../../materialui/components/PapperBlock/PapperBlock'
 import { login } from './../../../api';
 import './style.css';
 
-
-// validation functions
-// const required = value => (value == null ? 'Required' : undefined);
-// const email = value => (
-//     value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-//         ? 'Invalid email'
-//         : undefined
-// );
-
 class LoginForm extends React.Component {
   state = {
-    amount: '',
     password: '',
-    username: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
+    username: ''
   }
   componentWillMount() {
     // eslint-disable-next-line no-unused-expressions
@@ -46,13 +19,6 @@ class LoginForm extends React.Component {
 
   handleClickShowPassword = () => {
     this.setState(state => ({ showPassword: !state.showPassword }));
-  };
-  handleClickShowPassword = () => {
-    this.setState({ showPassword: !this.state.showPassword });
-  };
-
-  handleMouseDownPassword = event => {
-    event.preventDefault();
   };
 
   handleLogin = () => {
@@ -75,74 +41,37 @@ class LoginForm extends React.Component {
   }
 
   render() {
-    const {
-      classes,
-      handleSubmit,
-      pristine,
-      submitting
-    } = this.props;
     return (
-      <div className={classes.formWrap}>
-        <PapperBlock whiteBg title="" desc="">
-          <img className="rock-img" src='rokinapp.png' />
-          <div>
-            <FormControl className={classes.formControl}>
-            </FormControl>
+      <div className="main-container">
+        <div className="container">
+          <div className="d-flex justify-content-center h-100">
+            <img className="rockin"src="rokinapp.png" />
+            <div className="card">
+              <div className="card-header">
+                <h3>Sign In</h3>
+              </div>
+              <div className="card-body">
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-user" /></span>
+                  </div>
+                  <input type="text" className="form-control" placeholder="username" onChange={this.handleChange('username')} />
+                </div>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-key" /></span>
+                  </div>
+                  <input type="password" className="form-control" placeholder="password" onChange={this.handleChange('password')} />
+                </div>
+                <div className="form-group">
+                  <input type="submit" value="Login" className="login_btn" onClick={this.handleLogin} />
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <TextField
-              id="filled-adornment-password"
-              className="user-name"
-              variant="filled"
-              type="text"
-              label="username"
-              value={this.state.username}
-              onChange={this.handleChange('username')}
-            />
-            <br />
-            <br />
-            <TextField
-              defaultValue="Error"
-              id="filled-adornment-password"
-              className="user-name"
-              variant="filled"
-              type={this.state.showPassword ? 'text' : 'password'}
-              label="Password"
-              value={this.state.password}
-              onChange={this.handleChange('password')}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="Toggle password visibility"
-                      onClick={this.handleClickShowPassword} s
-                    >
-                      {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </div>
-          <br />
-          <div className={classes.btnArea}>
-            {/* <FormControlLabel control={<Field name="checkbox" component={Checkbox} />} label="Remember" /> */}
-            <Button variant="raised" color="primary" onClick={this.handleLogin}>
-              Continue
-                      <ArrowForward className={classNames(classes.rightIcon, classes.iconSmall)} disabled={submitting || pristine} />
-            </Button>
-          </div>
-        </PapperBlock>
+        </div>
       </div>
     );
   }
 }
-
-// LoginForm.propTypes = {
-//   classes: PropTypes.object.isRequired,
-//   handleSubmit: PropTypes.func.isRequired,
-//   pristine: PropTypes.bool.isRequired,
-//   submitting: PropTypes.bool.isRequired,
-// };
-
-export default withStyles(styles)(LoginForm);
+export default LoginForm;
