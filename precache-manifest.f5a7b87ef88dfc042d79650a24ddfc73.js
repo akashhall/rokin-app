0582ca92923e836d7e7032d581c4402320f20a7b
akashhall <41614662+akashhall@users.noreply.github.com>
2019-02-17 15:48:55 +0530

    Updates

---
+++ precache-manifest.f5a7b87ef88dfc042d79650a24ddfc73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52b56ef0865711fd0813d853aa0dceb0",
+    "url": "/rokin-app/static/media/restaurant-theme.52b56ef0.jpg"
+  },
+  {
+    "revision": "5159b0feae880de3e7ce",
+    "url": "/rokin-app/static/js/runtime~main.5159b0fe.js"
+  },
+  {
+    "revision": "59849a350ec60e53236f",
+    "url": "/rokin-app/static/js/main.59849a35.chunk.js"
+  },
+  {
+    "revision": "460649ce65963e09dd5b",
+    "url": "/rokin-app/static/js/2.460649ce.chunk.js"
+  },
+  {
+    "revision": "59849a350ec60e53236f",
+    "url": "/rokin-app/static/css/main.152caa41.chunk.css"
+  },
+  {
+    "revision": "c5b74daa26b23b4c4049e8dcd1af5991",
+    "url": "/rokin-app/index.html"
+  }
+];
